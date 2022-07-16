fa4bc63ef6efae05fc68edad4e712e78e76b6d14
Maher-rv <maher@revenyou.io>
2022-07-16 15:42:47 +0200

    clean up project layout

---
+++ lib/app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lepaya Assignment',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Lepaya Assignment'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: const [
+              Text('Hey there! Welcome to the Lepaya Flutter assignment.'),
+              SizedBox(height: 16),
+              Text(
+                'Check the `readme` of this repository for the instructions.',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
